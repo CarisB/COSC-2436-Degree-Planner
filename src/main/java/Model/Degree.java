633bbeb3307d0b79d7a30955b80d24b6223a5108
CarisB <chkbaek@gmail.com>
2023-04-26 23:59:57 -0500

    - Added getCoreCredits(), getMajorReqCredits(), getElectiveCredits()

--- src/main/java/Model/Degree.java
+++ src/main/java/Model/Degree.java
@@ -12,6 +12,9 @@ public class Degree {
 
     public int getId() { return id; }
     public String getName() { return name; }
+    public int getCoreCredits() { return coreCredits; }
+    public int getMajorReqCredits() { return majorReqCredits; }
+    public int getElectiveCredits() { return electiveCredits; }
     public Course[] getCore() { return core; }
     public Course[] getMajorReq() { return majorReq; }
     public Course[] getElective() { return elective; }
